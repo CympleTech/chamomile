7d9732b49f23b754a26f95824d8ce884824a0e81
sun <huachuang20@gmail.com>
2021-01-29 00:13:56 +0800

    fix delivery length = 0

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -5,7 +5,9 @@ pub mod types;
 #[macro_export]
 macro_rules! delivery_split {
     ($data:expr, $length:expr) => {
-        if $data.len() < $length {
+        if $length == 0 {
+            Vec::new()
+        } else if $data.len() < $length {
             $data.clone()
         } else {
             $data[0..$length].to_vec()
